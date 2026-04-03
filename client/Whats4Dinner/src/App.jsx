78d4f5605eaf474f6d9afae4a09aec3f28d39352
Grant Kostrzewa <grantkoz11@gmail.com>
2026-04-03 14:51:10 -0500

    Feat: Created basic dashboard UI

--- client/Whats4Dinner/src/App.jsx
+++ client/Whats4Dinner/src/App.jsx
@@ -3,16 +3,45 @@ import './App.css'
 import { fetchTestMessage } from './services/api'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [meal, setMeal] = useState("");
+  const [meals, setMeals] = useState([]);
 
   // Start fetching the test message when the component mounts
   useEffect(() => {
     fetchTestMessage().then(data => setMessage(data.message));
   }, []);
 
+  // Function to add a meal to the list
+  const addMeal = () => {
+    if (!meal) return;  // Do not add empty meals
+
+    // Add the new meal to the list of meals
+    setMeals([...meals, meal]);
+    setMeal("");
+  }
+
   return (
     <>
-      
+      <div >
+        <h1>Whats4Dinner?</h1>
+
+        <input>
+          type="text"
+          placeholder="Enter a meal"
+          value={meal}
+          onChange={(e) => setMeal(e.target.value)}
+        </input>
+        <button onClick={addMeal}>Add Meal</button>
+
+        <h2>Recent Meals</h2>
+        <ul>
+          {meals.map((m, index) => (
+            <li key={index}>{m}</li>
+          ))}
+        </ul>
+
+        <button>What's for dinner?</button>
+      </div>
     </>
   )
 }
