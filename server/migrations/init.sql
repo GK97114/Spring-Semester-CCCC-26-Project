c172edcf19fe25ead7aa7477d441d3edc1bc5d08
Grant Kostrzewa <grantkoz11@gmail.com>
2026-04-09 13:33:42 -0500

    refactor: adjusted tables and added resturants tables to migration table creation based on PRD

--- server/migrations/init.sql
+++ server/migrations/init.sql
@@ -8,15 +8,25 @@ CREATE TABLE users (
 CREATE TABLE meals (
     id UUID PRIMARY KEY,
     user_id UUID NOT NULL,
-    name TEXT NOT NULL,
+    location TEXT,
+    meal_name TEXT NOT NULL,
     cuisine TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    rating INTEGER CHECK (rating >= 1 AND rating <= 5),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    notes TEXT
 );
 
-CREATE TABLE favorite_meals (
+CREATE TABLE resturants (
     id UUID PRIMARY KEY,
+    name TEXT NOT NULL,
+    cuisine TEXT,
+    google_place_id TEXT UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE TABLE favorite_restaurants (
     user_id UUID NOT NULL,
-    meal_id UUID NOT NULL,
+    restaurant_id UUID NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE(user_id, meal_id)
+    UNIQUE(user_id, restaurant_id)
 );
