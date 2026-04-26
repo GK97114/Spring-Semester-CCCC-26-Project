d27c62492d36091a4f8c8767b4458a00e35781ea
Grant Kostrzewa <grantkoz11@gmail.com>
2026-04-26 14:52:53 -0500

    fix: Double server in path fix #1 in database migration script

--- server/scripts/init-db.js
+++ server/scripts/init-db.js
@@ -3,7 +3,7 @@ import fs from "fs";
 
 const initDb = async () => {
     try {
-        const sql = fs.readFileSync("./server/migrations/init.sql", "utf-8");
+        const sql = fs.readFileSync("./migrations/init.sql", "utf-8");
         await pool.query(sql);
         console.log("Database initialized successfully");
     } catch (err) {
