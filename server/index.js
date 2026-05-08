aba8e4202cfde6b65ed27d232a8f05245da3b0e5
Grant Kostrzewa <grantkoz11@gmail.com>
2026-05-08 15:29:30 -0500

    refactor: changed backend server to process Render's port instead of own port.

--- server/index.js
+++ server/index.js
@@ -29,6 +29,7 @@ app.use("/api/recommendations", recommendationRoutes);
 app.use("/api/meals", mealRoutes);
 
 // Start the server
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
